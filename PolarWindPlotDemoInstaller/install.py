1134384cefb16264d82f1e9590c3b2f44ae5ba59
gjr80 <gjroderick@gmail.com>
2022-06-16 14:33:13 +1000

    removed demo skin overrides from installer config

--- PolarWindPlotDemoInstaller/install.py
+++ PolarWindPlotDemoInstaller/install.py
@@ -35,46 +35,7 @@ polar_config = """
 [StdReport]
     [[PolarWindPlotDemo]]
         skin = PolarWindPlotDemo
-        [[[Units]]]
-            [[[[Groups]]]]
-                group_speed = km_per_hour
-            [[[[Labels]]]]
-                km_per_hour = km/h
-                knot = knots
-                meter_per_second = m/s
-                mile_per_hour = mph
-        [[[Labels]]]
-            compass_points = N, S, E, W
-            [[[[Generic]]]]
-                windGust = Gust Speed
-                windSpeed = Wind Speed
-        [[[PolarWindPlotGenerator]]]
-            image_background_image = None
-            image_width = 382
-            image_height = 361
-            image_background_circle_color = 0xF5F5F5
-            image_background_box_color = 0xF5C696
-            image_background_range_ring_color = 0xC3D9DD
-            plot_border = 5
-            legend_bar_width = 10
-            font_path = font/OpenSans-Bold.ttf
-            plot_font_size = 10
-            plot_font_color = 0x000000
-            legend_font_size = 10
-            legend_font_color = 0x000000
-            label_font_size = 12
-            label_font_color = 0x000000
-            plot_colors = aqua, 0xFF9900, 0xFF3300, 0x009900, 0x00CC00, 0x33FF33, 0x00FFCC
-            petal_width = 16
-            [[[[day_images]]]]
-                period = 86400
-                [[[[[daywindrose]]]]]
-                    plot_type = rose
-                    format = png
-                    [[[[[[windSpeed]]]]]]
-                        label = 24 Hour Wind Rose
-                        time_stamp = %H:%M %-d %b %y
-                        time_stamp_location = bottom, right
+        enable = False
 """
 
 # construct our config dict
